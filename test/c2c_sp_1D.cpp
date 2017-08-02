8bd5d8b623d8f6bed4854b7bcd5df80ce5e94598
E. G. Patrick Bos <egpbos@gmail.com>
2017-08-02 20:42:37 +0200

    Fix stricter Codacy issues

--- test/c2c_sp_1D.cpp
+++ test/c2c_sp_1D.cpp
@@ -1,20 +1,20 @@
 /*
  * xtensor-fftw
- * 
+ *
  * Copyright 2017 Patrick Bos
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 
 #include <stdexcept> // workaround for xt bug, where only including xarray does not include stdexcept; TODO: remove this include when bug is fixed!
@@ -26,7 +26,7 @@
 
 #include "gtest/gtest.h"
 
-TEST(fftC2CFloat1D,TransformAndInvert) {
+TEST(fftC2CFloat1D, TransformAndInvert) {
   xt::xarray<float> a = xt::arange<float>(8);
 
   auto a_fourier = xt::fftw::fft(a);
