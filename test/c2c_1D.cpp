51b7f7366fddd55167a501a69a155193bc5de523
E. G. Patrick Bos <egpbos@gmail.com>
2017-08-10 16:30:02 +0200

    Change license to BSD 3-clause

--- test/c2c_1D.cpp
+++ test/c2c_1D.cpp
@@ -1,20 +1,10 @@
 /*
  * xtensor-fftw
+ * Copyright (c) 2017, Patrick Bos
  *
- * Copyright 2017 Patrick Bos
+ * Distributed under the terms of the BSD 3-Clause License.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * The full license is in the file LICENSE, distributed with this software.
  */
 
 #include <stdexcept> // workaround for xt bug, where only including xarray does not include stdexcept; TODO: remove this include when bug is fixed!
