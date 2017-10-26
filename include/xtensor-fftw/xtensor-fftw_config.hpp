d8c77289bc937b8e4b5894318888eae647863b1d
E. G. Patrick Bos <egpbos@gmail.com>
2017-10-26 16:07:43 +0200

    Added installation target to cmake configuration
    Based on xtensor config.

---
+++ include/xtensor-fftw/xtensor-fftw_config.hpp
@@ -0,0 +1,16 @@
+/*
+ * xtensor-fftw
+ * Copyright (c) 2017, Patrick Bos
+ * Distributed under the terms of the BSD 3-Clause License.
+ *
+ * The full license is in the file LICENSE, distributed with this software.
+ */
+
+#ifndef XTENSOR_FFTW_CONFIG_HPP
+#define XTENSOR_FFTW_CONFIG_HPP
+
+#define XTENSOR_FFTW_VERSION_MAJOR 0
+#define XTENSOR_FFTW_VERSION_MINOR 1
+#define XTENSOR_FFTW_VERSION_PATCH 1
+
+#endif //XTENSOR_FFTW_CONFIG_HPP
