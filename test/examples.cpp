66b5246e284859294b6b53cc20218926abd0e89a
E. G. Patrick Bos <egpbos@gmail.com>
2017-11-29 17:04:17 +0100

    Added real life example test (derivative of sin)

---
+++ test/examples.cpp
@@ -0,0 +1,44 @@
+/*
+ * xtensor-fftw
+ * Copyright (c) 2017, Patrick Bos
+ * Distributed under the terms of the BSD 3-Clause License.
+ *
+ * The full license is in the file LICENSE, distributed with this software.
+ */
+
+// real life examples
+
+#include <xtensor-fftw/basic.hpp>
+#include <xtensor-fftw/helper.hpp>
+#include <xtensor/xarray.hpp>
+#include <complex>
+#include <xtensor/xbuilder.hpp>  // xt::arange
+#include <xtensor/xmath.hpp>  // xt::sin, cos
+#include <xtensor/xio.hpp>
+
+#include "gtest/gtest.h"
+
+
+TEST(examples, sin_derivative) {
+  // generate a sinusoid field
+  double dx = M_PI/100;
+  xt::xarray<double> x = xt::arange(0., 2*M_PI, dx);
+  xt::xarray<double> sin = xt::sin(x);
+
+  // transform to Fourier space
+  auto sin_fs = xt::fftw::rfft(sin);
+
+  // multiply by i*k
+  std::complex<double> i {0, 1};
+  auto k = xt::fftw::rfftscale<double>(sin.shape()[0], dx);
+  xt::xarray< std::complex<double> > sin_derivative_fs = xt::eval(i * k * sin_fs);
+
+  // transform back to normal space
+  auto sin_derivative = xt::fftw::irfft(sin_derivative_fs);
+
+  EXPECT_TRUE(xt::allclose(xt::cos(x), sin_derivative));
+//  std::cout << "x:              " << x << std::endl;
+//  std::cout << "sin:            " << sin << std::endl;
+//  std::cout << "cos:            " << xt::cos(x) << std::endl;
+//  std::cout << "sin_derivative: " << sin_derivative << std::endl;
+}
